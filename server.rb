d319c2b15a81ae27cd02c83af28104b5f0a778c7
brianstanwyck <brian@highgroove.com>
2012-10-15 18:31:07 -0400

    Initial commit

---
+++ server.rb
@@ -0,0 +1,69 @@
+require 'sinatra'
+require 'httparty'
+require 'json'
+
+LAST_FM_API_KEY = '18508165b781bb40ebbd6aa5401117e3'
+
+class LastFM
+  def initialize(api_key)
+    @api_key = api_key
+  end
+
+  def top_tags
+    url = "http://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key=#{@api_key}&format=json"
+
+    HTTParty.get(url).parsed_response["toptags"]["tag"]
+  end
+
+  def tag_tree(tag, depth)
+    {
+      name: tag,
+      children: similar_tags(tag)
+    }
+  end
+
+  def tag_artists(tag)
+    url = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=#{tag}&api_key=#{@api_key}&format=json"
+    HTTParty.get(url).parsed_response["topartists"]["artist"].map do |artist_hash|
+      {
+        name: artist_hash['name'],
+        image_url: artist_hash['image'].find { |image| image['size'] == 'large' }['#text']
+      }
+    end
+  end
+
+  private
+
+  def similar_tags(tag)
+    url = "http://ws.audioscrobbler.com/2.0/?method=tag.getsimilar&tag=#{tag}&api_key=#{@api_key}&format=json"
+    HTTParty.get(url).parsed_response["similartags"]["tag"].take(15).map do |tag_hash|
+      { name: tag_hash['name'] }
+    end
+  end
+end
+
+class TagTree
+  def initialize(depth)
+    @depth = depth
+  end
+end
+
+get '/top_tags.json' do
+  content_type :json
+  @last_fm = LastFM.new(LAST_FM_API_KEY)
+  @last_fm.top_tags.map { |hash| { name: hash['name'] } }.to_json
+end
+
+get '/tag_tree.json' do
+  content_type :json
+  @last_fm = LastFM.new(LAST_FM_API_KEY)
+  tag = params[:tag]
+  @last_fm.tag_tree(tag).to_json
+end
+
+get '/artists.json' do
+  content_type :json
+  @last_fm = LastFM.new(LAST_FM_API_KEY)
+  tag = params[:tag]
+  @last_fm.tag_artists(tag).to_json
+end
